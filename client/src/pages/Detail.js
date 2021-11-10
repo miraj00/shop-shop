abf4cdd78f36f6ec6bac91028a3dfc1d67fb35bc
miraj <miraj00@gmail.com>
2021-11-10 14:34:39 -0500

    Updating and deleting items in cart implemented

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -36,26 +36,43 @@ function Detail() {
   
   const { loading, data } = useQuery(QUERY_PRODUCTS);
   
-  const { products } = state;
+  const { products, cart } = state;
 
   const addToCart = () => {
+      const itemInCart = cart.find((cartItem) => cartItem._id === id);
+
+      if (itemInCart) {
+        dispatch({
+          type: UPDATE_CART_QUANTITY,
+          _id: id,
+          purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+        });
+      } else {
+        dispatch({
+          type: ADD_TO_CART,
+          product: { ...currentProduct, purchaseQuantity: 1 }
+        });
+      }
+  };
+
+  const removeFromCart = () => {
     dispatch({
-      type: ADD_TO_CART,
-      product: { ...currentProduct, purchaseQuantity: 1 }
+      type: REMOVE_FROM_CART,
+      _id: currentProduct._id
     });
   };
   
   useEffect(() => {
-    if (products.length) {
-      setCurrentProduct(products.find(product => product._id === id));
-    } 
-    else if (data) {
-      dispatch({
-        type: UPDATE_PRODUCTS,
-        products: data.products
-      });
-    }
-  }, [products, data, dispatch, id]);  
+      if (products.length) {
+        setCurrentProduct(products.find(product => product._id === id));
+      } 
+      else if (data) {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: data.products
+        });
+      }
+    }, [products, data, dispatch, id]);  
 
 
   return (
@@ -71,7 +88,11 @@ function Detail() {
           <p>
             <strong>Price:</strong>${currentProduct.price}{' '}
             <button onClick={addToCart}>Add to Cart</button>
-            <button>Remove from Cart</button>
+            <button
+                disabled={!cart.find(p => p._id === currentProduct._id)} 
+                onClick={removeFromCart}            
+               >
+                 Remove from Cart</button>
           </p>
 
           <img
